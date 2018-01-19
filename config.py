34fc312574078588a16a35185d7ef2ba5bccd0fd
iqt4 <github@iqt.de.vu>
2018-01-19 18:09:13 +0100

    Initial configuration class

---
+++ config.py
@@ -0,0 +1,119 @@
+import gnucashxml
+import json
+import datetime as dt
+
+
+class configuration(object):
+
+    # json style - to be read from/written to config file later
+    _gnc_accounts = {
+        "transaction": [
+            "Aktiva:Barvermögen:MLP:Abwicklungskonto",
+            "Aktiva:Barvermögen:Deutsche Bank:Depot maxblue 01"
+        ],
+        "money-market": [
+            "Aktiva:Barvermögen:MLP:Tagesgeldkonto"
+        ],
+        "interest": [
+            "Erträge:Zinsen:MLP Konten",
+            "Erträge:Zinsen:Tagesgeld MLP"
+        ],
+        "commission": [
+            "Aufwendungen:Bankgebühren:MLP:Wertpapierdepot",
+            "Aufwendungen:Bankgebühren:MLP:Konten",
+            "Aufwendungen:Bankgebühren:Deutsche Bank:Depot DB"
+        ],
+        "tax": [
+            "Aufwendungen:Steuern:Kapitalertragssteuer",
+            "Aufwendungen:Steuern:Solidaritätszuschlag",
+            "Aufwendungen:Steuern:Quellensteuer"
+        ],
+        "investment": "Aktiva:Investments:Wertpapierdepot",
+        "dividend": "Erträge:Dividende:Wertpapierdepot"
+    }
+
+    _ini = {
+        "gnc_file": "Haushalt_xml.gnucash",
+        "due_date": "2012-01-01"
+    }
+
+    def __init__(self, filename=None):
+        self.ini_file = filename
+        self.gnucash_file = None
+        self.due_date = None
+        self.accounts = {}
+        self.book = gnucashxml.Book(None, None)
+        self.stocks = None
+
+    def read_config(self):
+        self.gnucash_file = self._ini["gnc_file"]
+        self.due_date = dt.datetime.strptime(self._ini['due_date'], '%Y-%m-%d')
+
+    def open_book(self):
+        self.book = gnucashxml.from_filename(self.gnucash_file)
+        self._map_accounts()
+
+        # find all stocks from commodities, except currencies and template
+        self.stocks = [c for c in self.book.commodities
+                       if c.space not in ['ISO4217', 'template']]
+
+    def _map_accounts(self):
+        account_map = {a.fullname(): a for a in self.book.accounts}
+
+        for k, v in self._gnc_accounts.items():
+            if isinstance(v, list):
+                self.accounts[k] = [account_map[i] for i in v]
+            else:
+                self.accounts[k] = account_map[v]
+
+# piecash accounts
+import piecash
+
+
+def get_accounts(book):
+    accounts = dict()
+
+
+
+
+# ab hier überarbeiten
+# due_date = dt.datetime.strptime(ini['due_date'], '%Y-%m-%d')
+
+# para = {'gnc_file': ini['gnc_file'],
+#        'due_date': due_date,
+#        'gnc_stocklist': stocks}
+
+import sys
+
+
+def load_ini():
+    if len(sys.argv) > 1:
+        ini = sys.argv[1]
+    else:
+        ini = 'pc.json'
+
+    with open(ini, 'r') as f:
+        return json.load(f)
+
+
+def save_ini(para):
+    def to_json(python_object):
+        if isinstance(python_object, dt.datetime):
+            #            return {'__class__': 'datetime',
+            #                '__value__': python_object.date().isoformat()}
+            return python_object.date().isoformat()
+        elif isinstance(python_object, piecash.core.account.Account):
+            return python_object.fullname.encode('utf-8')
+        raise TypeError(repr(python_object) + ' is not JSON serializable')
+
+    para_exp = {k: v for k, v in para.items()
+                if k in ['gnc_file', 'due_date']}
+
+    accounts = {p: para[p] for p in para
+                if p in ['gnc_portfolio', 'gnc_dividend', 'gnc_account',
+                         'gnc_charge', 'gnc_tax', 'gnc_dividend', 'gnc_day2day']}
+
+    para_exp['accounts'] = accounts
+
+    with open('pc.json', 'w') as f:
+        json.dump(para_exp, f, ensure_ascii=False, indent=4, default=to_json)
---
+++ gc2pp.py
@@ -0,0 +1,335 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  gc_xml_pp.py
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+import datetime as dt
+import csv
+import re
+import difflib
+import gnucashxml
+import config
+
+
+def get_date(due_date):
+    while True:
+        info = 'Datum [{0}]: '.format(due_date.strftime('%d.%m.%Y'))
+        user_input = input(info)
+        try:
+            if user_input == '':
+                d = due_date
+            else:
+                d = dt.datetime.strptime(user_input, '%d.%m.%Y')
+            return d
+        except ValueError:
+            print('Falsches Datum')
+
+
+def get_transactions(splits, due_date):
+    transactions = [sp.transaction for sp in splits
+                    if sp.transaction.post_date.replace(tzinfo=None) >= due_date]
+
+    return sorted(list(set(transactions)), key=lambda tr: tr.post_date)
+
+
+def write_stocklist(stocklist):
+    header = ['ISIN', 'WKN', 'Ticker-Symbol', 'Wertpapiername', 'Währung', 'Notiz']
+
+    with open('stock.csv', 'wb') as f:
+        f_csv = csv.DictWriter(f, header, delimiter=';', quoting=csv.QUOTE_NONE)
+        f_csv.writeheader()
+
+        for stock in stocklist:
+            row = {'ISIN': stock.cusip,
+                   'Ticker-Symbol': stock.mnemonic,
+                   'Wertpapiername': stock.fullname,
+                   'Währung': 'EUR',
+                   'Notiz': stock.namespace}
+
+            f_csv.writerow(row)
+
+
+def export_investment(accounts, due_date):
+    header = ['Datum', 'Typ', 'Wert', 'Buchungswährung',
+              'Bruttobetrag', 'Währung Bruttobetrag', 'Wechselkurs',
+              'Gebühren', 'Steuern', 'Stück',
+              'ISIN', 'WKN', 'Ticker-Symbol', 'Wertpapiername',
+              'Notiz']
+
+    acc = ['commission', 'tax', 'bank']
+
+    with open('investment.csv', 'w') as f:
+        f_csv = csv.DictWriter(f, header, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+        f_csv.writeheader()
+
+        for stock_account in accounts['investment'].children:
+            transactions = get_transactions(stock_account.splits, due_date)
+            if not transactions:
+                continue
+
+            for tr in transactions:
+                val = dict.fromkeys(acc, 0)
+                stock_quantity = 0
+                stock_value = 0
+
+                for sp in tr.splits:
+                    if sp.account == stock_account:
+                        stock_quantity += sp.quantity
+                        stock_value += sp.value
+                    else:
+                        for a in acc:
+                            if sp.account in accounts[a]:
+                                val[a] += sp.value
+
+                if stock_quantity.is_zero():
+                    continue
+
+                if val['bank'] != 0 or stock_value == 0:  # Abrechnungskonto oder Incentiv
+                    if stock_quantity.is_signed():
+                        deal = 'Verkauf'
+                    else:
+                        deal = 'Kauf'
+                    total = abs(val['bank'])
+                else:
+                    if stock_quantity.is_signed():
+                        deal = 'Auslieferung'
+                    else:
+                        deal = 'Einlieferung'
+                    total = stock_value
+
+                stock = stock_account.commodity
+
+                row = {'Datum': tr.post_date.strftime('%Y-%m-%d'),
+                       'Typ': deal,
+                       'Wert': str(total),
+                       'Buchungswährung': 'EUR',
+                       'Gebühren': str(val['commission']),
+                       'Steuern': str(val['tax']),
+                       'Stück': str(abs(stock_quantity)),
+                       'ISIN': stock.cusip,
+                       'Ticker-Symbol': stock.mnemonic,
+                       'Wertpapiername': stock.fullname,
+                       'Notiz': tr.description}
+
+                f_csv.writerow(row)
+
+
+def write_transfer(f_csv, transaction, accounts):
+    value = 0
+    for sp in transaction.splits:
+        if sp.account in accounts['money-market']:
+            value += sp.value
+
+    row = {'Datum': transaction.post_date.strftime('%Y-%m-%d'),
+           'Buchungswährung': 'EUR',
+           'Notiz': transaction.description}
+
+    if value != 0:
+        if value > 0:
+            row['Typ'] = 'Umbuchung (Ausgang)'
+        else:
+            row['Typ'] = 'Umbuchung (Eingang)'
+        row['Wert'] = str(-value)
+        f_csv.writerow(row)
+
+
+def write_split(f_csv, transaction, accounts):
+    acc = ['commission', 'tax', 'interest', 'bank', 'money-market']
+    val = dict.fromkeys(acc, 0)
+
+    for sp in transaction.splits:
+        for a in acc:
+            if sp.account in accounts[a]:
+                val[a] += sp.value
+
+    delta = sum(val.values())
+
+    row = {'Datum': transaction.post_date.strftime('%Y-%m-%d'),
+           'Buchungswährung': 'EUR',
+           'Notiz': transaction.description}
+
+    if val['commission'] != 0:
+        if val['commission'] > 0:
+            row['Typ'] = 'Gebühren'
+        else:
+            row['Typ'] = 'Gebührenerstattung'  # to be checked
+        row['Wert'] = str(abs(val['commission']))
+        f_csv.writerow(row)
+
+    if val['tax'] != 0:
+        if val['tax'] > 0:
+            row['Typ'] = 'Steuern'
+        else:
+            row['Typ'] = 'Steuerrückerstattung'
+        row['Wert'] = str(abs(val['tax']))
+        f_csv.writerow(row)
+
+    if val['interest'] < 0:
+        row['Typ'] = 'Zinsen'
+        row['Wert'] = str(-val['interest'])
+        f_csv.writerow(row)
+
+    if delta != 0:
+        if delta > 0:
+            row['Typ'] = 'Einlage'
+        else:
+            row['Typ'] = 'Entnahme'
+        row['Wert'] = str(abs(delta))
+        f_csv.writerow(row)
+
+
+def write_dividend(f_csv, transaction, accounts):
+    junk = ['INHABER', 'NAMENS', 'VORZUGS', 'STAMM', 'AKTIEN',
+            'SHARES', 'REGISTERED']
+
+    stock_names = [s.fullname for s in accounts['stocks']]
+    stock = None
+    quantity = 0
+
+    # find stock by ISIN (MLP)
+    r = re.search(r'WKN [A-Z0-9]{6} / '
+                  r'(?P<isin>[A-Z]{2}[A-Z0-9]{9}[0-9])'
+                  r'.*?MENGE '
+                  r'(?P<quantity>[0-9]*)', transaction.description)
+
+    if r:
+        stock = accounts['stocks'](cusip=r.group('isin'))
+        quantity = r.group('quantity')
+    else:
+        # find stock by name (maxblue)
+        r = re.search(r'STK/NOM: '
+                      r'(?P<quantity>[0-9]*) '
+                      r'(?P<name>.*)', transaction.description)
+        if r:
+            quantity, name = r.groups()
+            for j in junk:
+                name = name.replace(j, '')
+
+            for s in difflib.get_close_matches(name, stock_names, 1):
+                stock = accounts['stocks'](fullname=s)
+
+    if stock:
+        tax = 0
+        dividend = 0
+        for sp in transaction.splits:
+            if sp.account == accounts['dividend']:
+                dividend = abs(sp.value)
+
+            elif sp.account in accounts['tax']:
+                tax += sp.value
+
+        row = {
+            'Datum': transaction.post_date.strftime('%Y-%m-%d'),
+            'Typ': 'Dividende',
+            'Wert': str(dividend - tax),
+            'Buchungswährung': 'EUR',
+            'Stück': quantity,
+            'ISIN': stock.cusip,
+            'Ticker-Symbol': stock.mnemonic,
+            'Wertpapiername': stock.fullname,
+            'Notiz': transaction.description
+        }
+        if tax:
+            row['Steuern'] = str(tax)
+
+        f_csv.writerow(row)
+
+
+def export_money(accounts, due_date):
+    tr_money = []
+    for a in accounts['money-market']:
+        tr_money += get_transactions(a.splits, due_date)
+
+    tr_bank = []
+    for a in accounts['bank']:
+        tr_bank += get_transactions(a.splits, due_date)
+
+    header = ['Datum', 'Typ', 'Wert', 'Buchungswährung', 'Steuern',
+              'Stück', 'ISIN', 'WKN', 'Ticker-Symbol', 'Wertpapiername',
+              'Notiz']
+
+    with open('money-market.csv', 'w') as f:
+        f_csv = csv.DictWriter(f, header, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+        f_csv.writeheader()
+
+        if not tr_money:
+            return
+
+        for tr in tr_money:
+            if tr not in tr_bank:  # Umbuchung already done
+                write_split(f_csv, tr, accounts)
+
+
+def export_bank(accounts, due_date):
+    tr_dividend = get_transactions(accounts['dividend'].splits, due_date)
+
+    tr_investment = []
+    for a in accounts['investment'].children:
+        tr_investment += get_transactions(a.splits, due_date)
+
+    tr_bank = []
+    for a in accounts['bank']:
+        tr_bank += get_transactions(a.splits, due_date)
+
+    header = ['Datum', 'Typ', 'Wert', 'Buchungswährung', 'Steuern',
+              'Stück', 'ISIN', 'WKN', 'Ticker-Symbol', 'Wertpapiername',
+              'Notiz']
+
+    with open('bank.csv', 'w') as f:
+        f_csv = csv.DictWriter(f, header, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+        f_csv.writeheader()
+
+        if not tr_bank:
+            return
+
+        for tr in tr_bank:
+            if tr in tr_investment:
+                continue
+            elif tr in tr_dividend:
+                write_dividend(f_csv, tr, accounts)
+            else:
+                write_transfer(f_csv, tr, accounts)
+                write_split(f_csv, tr, accounts)
+
+
+def main():
+    # with piecash.open_book(config.ini['gnc_file'], open_if_lock=True) as book:
+    conf = config.configuration()
+    conf.read_config()
+    conf.open_book()
+
+
+    export_bank(accounts, due_date)
+    export_money(accounts, due_date)
+    export_investment(accounts, due_date)
+
+    #        write_dividend(para)
+    #        write_stocklist(stocklist)
+
+    exit()
+
+        # para['due_date'] = dt.datetime.now()
+
+        # save_ini(para)
+    return 0
+
+
+if __name__ == '__main__':
+    main()
