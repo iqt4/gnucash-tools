e5cdc211cfc000e1c1a6f6c72e4f22ed23e3707a
iqt4 <gitub@iqt.de.vu>
2020-02-16 21:26:16 +0100

    Introduce AccountType(Enum)

--- config.py
+++ config.py
@@ -1,19 +1,37 @@
 import json
+from enum import Enum
 import argparse
 import gnucashxml
 # import piecash
 import datetime as dt
 
 
+class AccountType(Enum):
+    DEPOSIT = "deposit"
+    PORTFOLIO = "portfolio"
+    FEE = "fee"
+    TAX = "tax"
+    DIVIDEND = "dividend"
+    INTEREST = "interest"
+
+
+def get_transactions_from_splits(splits, due_date):
+    # use a set to avoid doubles
+    transactions = {sp.transaction for sp in splits
+                    if sp.transaction.post_date.replace(tzinfo=None) >= due_date}
+
+    return transactions
+
+
 class Configuration(object):
 
     def __init__(self, filename=None, due_date=None):
         self.conf_file = filename
-        self._conf_json = None
-        self.gnc_file = None
         self.due_date = due_date
+        self.gnc_file = None
+        self._conf_json = None
         self._gnc_accounts = None
-        self.accounts = {}
+        self.accounts = {account_type: [] for account_type in AccountType}
         self.stocks = None
 
         self.read_config()
@@ -25,6 +43,7 @@ class Configuration(object):
         self.gnc_file = self._conf_json['gnc_file']
         self.due_date = dt.datetime.strptime(self._conf_json['due_date'], '%Y-%m-%d')
         self._gnc_accounts = self._conf_json['accounts']
+
         # Todo sql versus xml
         # with piecash.open_book(config.ini['gnc_file'], open_if_lock=True) as book:
         self.read_book_xml()
@@ -32,12 +51,23 @@ class Configuration(object):
     def read_book_xml(self):
         book = gnucashxml.from_filename(self.gnc_file)
         account_map = {a.fullname: a for a in book.accounts}
-        # Todo KeyError
-        for k, v in self._gnc_accounts.items():
-            if isinstance(v, list):
-                self.accounts[k] = [account_map[i] for i in v]
-            else:
-                self.accounts[k] = account_map[v]
+        for g in self._gnc_accounts:
+            # Todo KeyError
+            account_type = AccountType(g['type'])
+            account_name = g['gnc']
+            account = account_map[account_name]
+            self.accounts[account_type].append(account)
+
+        tr_deposit = set()
+        for account in self.accounts[AccountType.DEPOSIT]:
+            transactions = get_transactions_from_splits(account.splits, self.due_date)
+            tr_deposit.update(transactions)
+
+        tr_investment = set()
+        for portfolio in self.accounts[AccountType.PORTFOLIO]:
+            for security in portfolio.children:
+                transactions = get_transactions_from_splits(security.splits, self.due_date)
+                tr_investment.update(transactions)
 
         # find all stocks from commodities, except currencies and template
         self.stocks = [c for c in book.commodities
@@ -57,6 +87,59 @@ def get_configuration():
 # para = {'gnc_file': ini['gnc_file'],
 #        'due_date': due_date,
 #        'gnc_stocklist': stocks}
+
+# account.BUY             = Kauf
+# account.DEPOSIT         = Einlage
+# account.DIVIDENDS       = Dividende
+# account.FEES            = Geb\u00FChren
+# account.FEES_REFUND     = Geb\u00FChrenerstattung
+# account.INTEREST        = Zinsen
+# account.INTEREST_CHARGE = Zinsbelastung
+# account.REMOVAL         = Entnahme
+# account.SELL            = Verkauf
+# account.TAXES           = Steuern
+# account.TAX_REFUND      = Steuerr\u00FCckerstattung
+# account.TRANSFER_IN     = Umbuchung (Eingang)
+# account.TRANSFER_OUT    = Umbuchung (Ausgang)
+#
+# event.STOCK_SPLIT = Aktiensplit
+#
+# portfolio.BUY               = Kauf
+# portfolio.DELIVERY_INBOUND  = Einlieferung
+# portfolio.DELIVERY_OUTBOUND = Auslieferung
+# portfolio.SELL              = Verkauf
+# portfolio.TRANSFER_IN       = Umbuchung (Eingang)
+# portfolio.TRANSFER_OUT      = Umbuchung (Ausgang)
+
+# CSVColumn_AccountName = Konto
+# CSVColumn_AccountName2nd = Gegenkonto
+# CSVColumn_CumulatedPerformanceInPercent = Kumulierte Performance in %
+# CSVColumn_Currency = W\u00E4hrung
+# CSVColumn_CurrencyGrossAmount = W\u00E4hrung Bruttobetrag
+# CSVColumn_Date = Datum
+# CSVColumn_DateQuote = Kursdatum
+# CSVColumn_DateValue = Datum Wert
+# CSVColumn_DeltaInPercent = Delta in %
+# CSVColumn_ExchangeRate = Wechselkurs
+# CSVColumn_Fees = Geb\u00FChren
+# CSVColumn_GrossAmount = Bruttobetrag
+# CSVColumn_ISIN = ISIN
+# CSVColumn_InboundTransferals = Zug\u00E4nge
+# CSVColumn_Note = Notiz
+# CSVColumn_OutboundTransferals = Abg\u00E4nge
+# CSVColumn_PortfolioName = Depot
+# CSVColumn_PortfolioName2nd = Gegendepot
+# CSVColumn_Quote = Kurs
+# CSVColumn_SecurityName = Wertpapiername
+# CSVColumn_Shares = St\u00FCck
+# CSVColumn_Taxes = Steuern
+# CSVColumn_TickerSymbol = Ticker-Symbol
+# CSVColumn_Time = Uhrzeit
+# CSVColumn_TransactionCurrency = Buchungsw\u00E4hrung
+# CSVColumn_Transferals = Zu-/Abg\u00E4nge
+# CSVColumn_Type = Typ
+# CSVColumn_Value = Wert
+# CSVColumn_WKN = WKN
 
 def save_ini(para):
     def to_json(python_object):
